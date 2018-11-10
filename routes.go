a137314125b2292a0b48517351bf61bac476bd36
Matt Behnke <matthewbehnke0@gmail.com>
2018-11-10 15:14:51 -0600

    added match data to be sent back when looking at an event

--- routes.go
+++ routes.go
@@ -10,6 +10,18 @@ import (
 type Event struct {
 	EventID   int
 	EventName string
+	Match     []Matches
+}
+
+type Matches struct {
+	MatchID         int
+	TeamName        string
+	TeamNumber      int
+	EventName       string
+	MatchScoreOne   int
+	MatchScoreTwo   int
+	MatchScoreThree int
+	Year            int
 }
 
 var healthOK = fmt.Sprintf("{\"message\":\"ok\", \"version\":\"%s\"}\n", versionNumber)
@@ -80,47 +92,99 @@ func addevent(db *sql.DB) gin.HandlerFunc {
 
 func getsingleevent(db *sql.DB) gin.HandlerFunc {
 	fn := func(c *gin.Context) {
-		name := c.Param("name")
-		rows, err := db.Query("SELECT * FROM events WHERE eventname = ?", name)
-		handleErr(400, err, c)
+		EventName := c.Param("name")
+		EventRows, EventErr := db.Query("SELECT * FROM events WHERE eventName=?", EventName)
+		handleErr(400, EventErr, c)
 		var arrayEvent []Event
-		for rows.Next() {
+		for EventRows.Next() {
 			var eventID int
 			var eventName string
-			err = rows.Scan(&eventID, &eventName)
-			handleErr(400, err, c)
+			EventErr = EventRows.Scan(&eventID, &eventName)
+			handleErr(400, EventErr, c)
+			MatchRows, MatchErr := db.Query("SELECT * FROM matches WHERE eventName= ?", eventName)
+			handleErr(400, MatchErr, c)
+			var matchesArray []Matches
+			for MatchRows.Next() {
+				var matchID int
+				var teamName string
+				var teamNumber int
+				var eventName string
+				var matchScoreOne int
+				var matchScoreTwo int
+				var matchScoreThree int
+				var year int
+				MatchErr = MatchRows.Scan(&matchID, &teamName, &teamNumber, &eventName, &matchScoreOne, &matchScoreTwo, &matchScoreThree, &year)
+				handleErr(400, MatchErr, c)
+				f := Matches{
+					MatchID:         matchID,
+					TeamName:        teamName,
+					TeamNumber:      teamNumber,
+					EventName:       eventName,
+					MatchScoreOne:   matchScoreOne,
+					MatchScoreTwo:   matchScoreTwo,
+					MatchScoreThree: matchScoreThree,
+					Year:            year,
+				}
+				matchesArray = append(matchesArray, f)
+			}
 			b := Event{
 				EventID:   eventID,
 				EventName: eventName,
+				Match:     matchesArray,
 			}
 			arrayEvent = append(arrayEvent, b)
 		}
 		c.JSON(200, arrayEvent)
 	}
 	return gin.HandlerFunc(fn)
-
 }
 
 func getallevents(db *sql.DB) gin.HandlerFunc {
 	fn := func(c *gin.Context) {
-		rows, err := db.Query("SELECT * FROM events")
-		handleErr(400, err, c)
+		EventRows, EventErr := db.Query("SELECT * FROM events")
+		handleErr(400, EventErr, c)
 		var arrayEvent []Event
-		for rows.Next() {
+		for EventRows.Next() {
 			var eventID int
 			var eventName string
-			err = rows.Scan(&eventID, &eventName)
-			handleErr(400, err, c)
+			EventErr = EventRows.Scan(&eventID, &eventName)
+			handleErr(400, EventErr, c)
+			MatchRows, MatchErr := db.Query("SELECT * FROM matches WHERE eventName= ?", eventName)
+			handleErr(400, MatchErr, c)
+			var matchesArray []Matches
+			for MatchRows.Next() {
+				var matchID int
+				var teamName string
+				var teamNumber int
+				var eventName string
+				var matchScoreOne int
+				var matchScoreTwo int
+				var matchScoreThree int
+				var year int
+				MatchErr = MatchRows.Scan(&matchID, &teamName, &teamNumber, &eventName, &matchScoreOne, &matchScoreTwo, &matchScoreThree, &year)
+				handleErr(400, MatchErr, c)
+				f := Matches{
+					MatchID:         matchID,
+					TeamName:        teamName,
+					TeamNumber:      teamNumber,
+					EventName:       eventName,
+					MatchScoreOne:   matchScoreOne,
+					MatchScoreTwo:   matchScoreTwo,
+					MatchScoreThree: matchScoreThree,
+					Year:            year,
+				}
+				matchesArray = append(matchesArray, f)
+			}
 			b := Event{
 				EventID:   eventID,
 				EventName: eventName,
+				Match:     matchesArray,
 			}
 			arrayEvent = append(arrayEvent, b)
 		}
 		c.JSON(200, arrayEvent)
 	}
 	return gin.HandlerFunc(fn)
-
 }
 
 func handleErr(errorCode int, err error, c *gin.Context) {
